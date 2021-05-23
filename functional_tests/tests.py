ecf4e885335a5ad40d047f303f9b1257e3d02b66
yuanyuyu <yuanyuyu@yuanyuyudeMacBook-Pro.local>
2021-05-23 14:52:27 +0800

    move sqlit database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,7 +12,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		staging_server = os.environ.get('STAGING_SERVER')
-		if  staging_server:
+		if staging_server:
 			self.live_server_url = 'http://' + staging_server
 
 	def tearDown(self):
